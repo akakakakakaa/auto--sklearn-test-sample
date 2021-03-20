84db003f35f7f4f910196bc9fa7f166293aefa1d
kihwan kim <juljin1875@gmail.com>
2021-03-20 13:53:19 +0900

    [Refactor] change the globe from pixel units to viewport units

--- nodejs/src/widgets/common/HyperGlobe.tsx
+++ nodejs/src/widgets/common/HyperGlobe.tsx
@@ -8,9 +8,9 @@ function getDirection(step: number) {
 }
 
 function getSize(step: number) {
-  if (step === 1) return 250;
-  else if (step === 2) return 450;
-  else if (step === 3) return 650;
+  if (step === 1) return 14;
+  else if (step === 2) return 25;
+  else if (step === 3) return 33;
   else return 0;
 }
 
@@ -23,43 +23,46 @@ function getColor(step: number) {
 
 export default function HyperGlobe(props: { step: number }) {
   return (
-    <div
-      className="hyperglobe"
-      style={{
-        width: getSize(props.step),
-        height: getSize(props.step),
-        margin: -getSize(props.step) / 2,
-      }}
-    >
-      {[...Array(21)].map((_) => {
-        return (
-          <span
-            style={{
-              opacity: props.step / 5,
-              animationDirection: getDirection(props.step),
-              border: `1px solid ${getColor(props.step)}`,
-            }}
-          ></span>
-        );
-      })}
+    <>
+      <div
+        className="hyperglobe"
+        style={{
+          width: `${getSize(props.step)}vw`,
+          height: `${getSize(props.step)}vw`,
+          margin: `${-getSize(props.step) / 2}vw`,
+        }}
+      >
+        {[...Array(21)].map((_) => {
+          return (
+            <span
+              style={{
+                opacity: props.step / 5,
+                animationDirection: getDirection(props.step),
+                border: `1px solid ${getColor(props.step)}`,
+              }}
+            ></span>
+          );
+        })}
+      </div>
       {props.step === 3 ? (
         <Typography
+          className="start"
           variant="h1"
           display="inline"
           style={{
             position: "absolute",
             top: "50%",
-            left: "50%",
+            left: "77%",
             transform: "translate(-50%, -50%)",
             color: "rgb(238, 198, 94)",
             fontFamily: "LAB디지털",
-            width: "100%",
+            /*width: "100%",*/
             textAlign: "center",
           }}
         >
           S T A R T
         </Typography>
       ) : null}
-    </div>
+    </>
   );
 }
