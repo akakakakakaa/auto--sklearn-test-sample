efcd19ad394f298845ba27658829169cb54e138f
kihwan kim <juljin1875@gmail.com>
2021-03-23 09:59:33 +0900

    [New Feature] Read CSV from Dropzone

--- nodejs/src/widgets/create/components/CreateStepper.tsx
+++ nodejs/src/widgets/create/components/CreateStepper.tsx
@@ -5,12 +5,10 @@ import Step from "@material-ui/core/Step";
 import StepLabel from "@material-ui/core/StepLabel";
 import StepContent from "@material-ui/core/StepContent";
 import Button from "@material-ui/core/Button";
-import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import CheckIcon from "@material-ui/icons/Check";
 import SettingsIcon from "@material-ui/icons/Settings";
 import TrackChangesIcon from "@material-ui/icons/TrackChanges";
-import DataObjectSelectTable from "./DataObjectSelectTable";
 import DataObjectSampleTable from "./DataObjectSampleTable";
 import TaskSelector from "./TaskSelector";
 import { taskType } from "../../../common/Constants";
@@ -54,60 +52,16 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-interface Data {
-  id: string;
-  dataObject: string;
-  dataType: string;
-  author: string;
-  dataSource: string;
-  createdOn: string;
-  lastModified: string;
-}
-
-function createData(
-  id: string,
-  dataObject: string,
-  dataType: string,
-  author: string,
-  dataSource: string,
-  createdOn: string,
-  lastModified: string
-): Data {
-  return {
-    id,
-    dataObject,
-    dataType,
-    author,
-    dataSource,
-    createdOn,
-    lastModified,
-  };
-}
-
-const taskMap = {
-  0: "regression",
-  1: "classification",
-  4: "timeseires",
-};
-
 export default function CreateStepper() {
   const classes = useStyles();
   const [activeStep, setActiveStep] = useState(0);
-  const [selected, setSelected] = useState<string[]>([]); // DataObject Name
-  const [selectedId, setSelectedId] = useState("-1"); // DataObject ID
   const [uploadedCSV, setCSV] = useState<File>();
   const [selectedColumn, setSelectedColumn] = useState("");
   const [expName, setExpName] = useState("");
   const [task, setTask] = useState<taskType>("classification");
-  //const history = useHistory();
-  const [doList, setDoList] = useState<Data[]>([]);
   const [sampleColumns, setSampleColumns] = useState<string[]>([]);
   const [sampleRows, setSampleRows] = useState<string[][]>([]);
-  const [numUniqueVal, setNumUniqueVal] = useState(-1);
   const [isTraining, setIsTraining] = useState(false);
-
-  console.dir(uploadedCSV);
-
   const setSample = (c: string[], r: string[][]) => {
     setSampleColumns(c);
     setSampleRows(r);
@@ -122,10 +76,6 @@ export default function CreateStepper() {
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
-
-  //const handleHome = () => {
-  //  history.push("/experiments");
-  //};
 
   const stepIds = Array(3)
     .fill(0)
@@ -184,124 +134,17 @@ export default function CreateStepper() {
   }
 
   useEffect(() => {
-    /*getDataObjects()
-      .then((data) =>
-        data.map(
-          (
-            row: {
-              id: string;
-              name: string;
-              type: string;
-              author: string;
-              source: string;
-              created_on: string;
-              last_edited: string;
-            },
-            index: number
-          ) =>
-            createData(
-              row.id,
-              row.name,
-              row.type,
-              row.author,
-              row.source,
-              row.created_on,
-              row.last_edited
-            )
-        )
-      )
-      .then((DOs: Data[]) => setDoList(DOs));*/
-    setDoList([
-      createData(
-        "1",
-        "테스트 데이터 1",
-        "File",
-        "Simulacre7",
-        "test1.csv",
-        "1994.02.21 23:59:59",
-        "2100.12.25 23:59:59"
-      ),
-      createData(
-        "2",
-        "테스트 데이터 2",
-        "File",
-        "Simulacre7",
-        "test1.csv",
-        "2222.02.21 23:59:59",
-        "3333.12.25 23:59:59"
-      ),
-      createData(
-        "3",
-        "테스트 데이터 3",
-        "File",
-        "Simulacre7",
-        "test3.csv",
-        "4444.02.21 23:59:59",
-        "5555.12.25 23:59:59"
-      ),
-      createData(
-        "4",
-        "테스트 데이터 4",
-        "File",
-        "Simulacre7",
-        "test4.csv",
-        "4444.02.21 23:59:59",
-        "5555.12.25 23:59:59"
-      ),
-      createData(
-        "5",
-        "테스트 데이터 5",
-        "File",
-        "Simulacre7",
-        "test5.csv",
-        "4444.02.21 23:59:59",
-        "5555.12.25 23:59:59"
-      ),
-      createData(
-        "6",
-        "테스트 데이터 6",
-        "File",
-        "Simulacre7",
-        "test6.csv",
-        "4444.02.21 23:59:59",
-        "5555.12.25 23:59:59"
-      ),
-      createData(
-        "7",
-        "테스트 데이터 7",
-        "File",
-        "Simulacre7",
-        "test7.csv",
-        "4444.02.21 23:59:59",
-        "5555.12.25 23:59:59"
-      ),
-    ]);
-  }, []);
-
-  useEffect(() => {
-    /*if (selectedId !== "-1") {
-      getDataObjectSample(selectedId).then((data) =>
-        setSample(data.columns, data.rows)
-      );
-    }*/
-    setSample(
-      ["col 1", "col 2", "col 3", "col 4", "col 5"],
-      [
-        [selectedId, selectedId, selectedId, selectedId, selectedId],
-        ["0", "0", "0", "0", "0"],
-        ["0", "0", "0", "0", "0"],
-        ["0", "0", "0", "0", "0"],
-        ["0", "0", "0", "0", "0"],
-      ]
-    );
-  }, [selectedId]);
-
-  /*useEffect(() => {
-    getRecommendedTask(selectedId, selectedColumn).then((data) => {
-      setTask(taskMap[data.task_type]);
-      setNumUniqueVal(data.num_unique_val);
-    });
-  }, [selectedColumn]);*/
+    const readCSV = async () => {
+      const txt = await uploadedCSV?.text();
+      if (!!txt) {
+        const txtArr = txt.split("\n");
+        const column = txtArr[0].split(",");
+        const row = txtArr.slice(1, 5).map((t) => t.split(","));
+        setSample(column, row);
+      }
+    };
+    readCSV();
+  }, [uploadedCSV]);
 
   const stepper = (
     <div className={classes.root}>
@@ -354,7 +197,6 @@ export default function CreateStepper() {
       {activeStep + 1 >= 3 ? (
         <HyperGlobe step={3} setIsTraining={setIsTraining} />
       ) : null}
-      {/*isTraining ? null : stepper*/}
       <Animated.div
         show={!isTraining}
         unmountAnim={` 
